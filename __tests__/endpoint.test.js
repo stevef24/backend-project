1d1c3fcdefb93b34395a54402632b061135013b3
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-02-20 15:43:34 +0000

    task-4 completed

--- __tests__/endpoint.test.js
+++ __tests__/endpoint.test.js
@@ -3,6 +3,7 @@ const db = require("../db/connection");
 const request = require("supertest");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/index");
+const { toBeSorted, toBeSortedBy } = require("jest-sorted");
 
 beforeEach(() => {
 	return seed(testData);
@@ -30,5 +31,55 @@ describe("GET /api/categories", () => {
 	});
 	it("404 response with bad request when give invalid endpoint ", () => {
 		return request(app).get("/api/categoriesasd").expect(404);
+	});
+});
+
+describe("GET /api/reviews", () => {
+	it("should return 200 ", () => {
+		return request(app)
+			.get("/api/reviews")
+			.expect(200)
+			.then(({ body }) => {
+				const reviews = body.reviews;
+				console.log(reviews, "<-----------");
+				expect(reviews.length).toBe(13);
+				expect(reviews).toBeSortedBy("created_at", { descending: true });
+				reviews.forEach((review) => {
+					expect(review).toHaveProperty("owner", expect.any(String));
+					expect(review).toHaveProperty("title", expect.any(String));
+					expect(review).toHaveProperty("review_id", expect.any(Number));
+					expect(review).toHaveProperty("category", expect.any(String));
+					expect(review).toHaveProperty("review_img_url", expect.any(String));
+					expect(review).toHaveProperty("created_at", expect.any(String));
+					expect(review).toHaveProperty("designer", expect.any(String));
+					expect(review).toHaveProperty("votes", expect.any(Number));
+					expect(review).toHaveProperty("comment_count", expect.any(String));
+				});
+			});
+	});
+	it("should return the correct data from the request", () => {
+		return request(app)
+			.get("/api/reviews")
+			.expect(200)
+			.then(({ body }) => {
+				const reviews = body.reviews;
+				console.log(reviews, "<-----------");
+				expect(reviews.length).toBe(13);
+				expect(reviews).toBeSortedBy("created_at", { descending: true });
+				reviews.forEach((review) => {
+					expect(review).toHaveProperty("owner", expect.any(String));
+					expect(review).toHaveProperty("title", expect.any(String));
+					expect(review).toHaveProperty("review_id", expect.any(Number));
+					expect(review).toHaveProperty("category", expect.any(String));
+					expect(review).toHaveProperty("review_img_url", expect.any(String));
+					expect(review).toHaveProperty("created_at", expect.any(String));
+					expect(review).toHaveProperty("designer", expect.any(String));
+					expect(review).toHaveProperty("votes", expect.any(Number));
+					expect(review).toHaveProperty("comment_count", expect.any(String));
+				});
+			});
+	});
+	it("404 err if the url path is incorrect", () => {
+		return request(app).get("/api/reviewsas").expect(404);
 	});
 });
